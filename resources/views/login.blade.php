839dd3e32d202b5b7d2e7d00a12688e7199c02c6
Lusia Hingi Bolen <tiaariantobyy@gmail.com>
2025-06-11 17:53:28 +0700

    Perbaikan 2

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="UTF-8"> 
     <title>Login Donasi Buku</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" rel="stylesheet">
     <style>
